6d2a9047b4b4dc0f9afe6fc34a110922dc562903
luishemanotf-gif <luishemanotf@gmail.com>
2026-05-12 12:40:56 -0500

    Create servicio.py

---
+++ servicio.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+
+class Servicio(ABC):
+
+    def __init__(self, nombre, precio_base):
+        self.nombre = nombre
+        self.precio_base = precio_base
+
+    @abstractmethod
+    def calcular_costo(self, horas):
+        pass
+
+    @abstractmethod
+    def descripcion(self):
+        pass
